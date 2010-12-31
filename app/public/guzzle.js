75f02cc23aaaa6349cac870d766d846721b04ccb
dan fichter <daniel.fichter@gmail.com>
2010-12-31 01:22:23 -0500

    display across and down word numbers

--- app/public/guzzle.js
+++ app/public/guzzle.js
@@ -3,22 +3,24 @@ $(function() {
         $.post('newPuzzle', {'newPuzzleSize' : $('#newPuzzleSize').val()},
             function (puzzle) {
                 $('#puzzleGenerationAttempts').text('It took ' + puzzle.numAttemptsToGenerate + ' attempts to generate this valid puzzle.');
-                redrawGrid(puzzle);
+                redraw(puzzle);
             }
         );
     });
 });
 
-function redrawGrid(puzzle) {
+function redraw(puzzle) {
 
-    $('#gridContainer').remove();
+    $('#puzzleContainer').remove();
+    var puzzleContainer = $('<div />', {id : 'puzzleContainer'}).appendTo('body');
+    puzzleContainer.data('puzzle', puzzle);
 
     var gridContainer = $('<div />',
         { width : 40 * puzzle.size,
           height : 40 * puzzle.size,
           class : 'gridContainer',
           id : 'gridContainer'
-        }).appendTo('body');
+        }).appendTo(puzzleContainer);
 
     for (var rowIdx = 0; rowIdx < puzzle.size; rowIdx++) {
         for (var colIdx = 0; colIdx < puzzle.size; colIdx++) {
@@ -40,6 +42,33 @@ function redrawGrid(puzzle) {
         }
     }
 
-    gridContainer.data('puzzle', puzzle);
+    var cluesContainer = $('<div />',
+      { id : 'cluesContainer',
+        class : 'cluesContainer'
+      }).appendTo(puzzleContainer);
+
+    [['acrossCluesContainer', 'Across', true],
+     ['downCluesContainer', 'Down', false]].forEach(function () {
+        var containerId = arguments[0][0];
+        var containerTitle = arguments[0][1];
+        var isAcross = arguments[0][2];
+        var container = $('<div />',
+          { id : containerId,
+            class : containerId,
+            text : containerTitle
+          }).appendTo(cluesContainer);
+        puzzle.slots.sort(function (slotA, slotB) {
+            return cellAt(puzzle, slotA).number > cellAt(puzzle, slotB).number ? 1 : -1;
+        }).forEach(function (slot) {
+            if (slot.isAcross == isAcross) {
+                var wordNumber = cellAt(puzzle, slot).number;
+                $('<div />', {text : wordNumber}).appendTo(container);
+            }
+        });
+    });
+
+    function cellAt(puzzle, slot) {
+        return puzzle.grid[slot.startCoord[0]][slot.startCoord[1]];
+    }
 
 }
